445bbca17e4beab0d2a46550fb8d2847e924784a
JP Vendramini <joaopedrovendraminiv90@gmail.com>
2021-05-27 21:35:35 -0300

    Connection Factory deleted

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -1,6 +1,5 @@
 package Application;
 import telas.FrameCadastro;
-import connectionDAO.FactoryConnection;
 
 public class Main {
     public static void main(String[] args) {
--- src/connectionDAO/FactoryConnection.java
+++
@@ -1,9 +0,0 @@
-package connectionDAO;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException; 
-        
-public class FactoryConnection {    
-    static String driverJDBC
-}
